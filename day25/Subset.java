973a7c170ab4a08dab833f2b4d1eb792a6d62a2a
rohinth043-coder <rohinth043@gmail.com>
2026-07-21 17:11:48 +0530

    rohinth

--- day25/Subset.java
+++ day25/Subset.java
@@ -1,16 +1,17 @@
-class Solution {
-    private void helper(int i, int nums[], List<Integer> subset, List<List<Integer>> result) {
+class ListNode {
+    public Static void main(int i, int n[], List<Integer> subset, List<List<Integer>> result) {
+        int n= nums;
         int n = nums.length;
-        if(i == nums.length) {
+        if(i == n.length) {
             result.add(new ArrayList<>(subset));
             return;
         }
-        subset.add(nums[i]);
-        helper(i + 1, nums, subset, result);
+        subset.add(n[i]);
+        helper(i + 1, n, subset, result);
         subset.remove(subset.size() - 1);
 
-        while(i + 1 < nums.length && nums[i] == nums[i + 1]) i++;
-        helper(i + 1, nums, subset, result);
+        while(i + 1 < n.length && n[i] == n[i + 1]) i++;
+        helper(i + 1, n, subset, result);
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
