03be7aad233ce64b97ee3243ddae822367821683
rohinth043-coder <rohinth043@gmail.com>
2026-07-23 21:31:25 +0530

    rohinth

---
+++ day29/problem3.java
@@ -0,0 +1,40 @@
+package day29;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class problem3 {
+    class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> pre = new HashMap<>();
+        for (int i = 0; i < numCourses; i++) pre.put(i, new ArrayList<>());
+        for (int[] edge : prerequisites) {
+            int course = edge[0];
+            int prerequisite = edge[1];
+            pre.get(course).add(prerequisite);
+        }
+
+        Set<Integer> taken = new HashSet<>();
+        for (int course = 0; course < numCourses; course++) {
+            if (!dfs(course, pre, taken)) return false;
+        }
+        return true;
+    }
+
+    private boolean dfs(int course, Map<Integer, List<Integer>> pre, Set<Integer> taken) {
+        if (pre.get(course).isEmpty()) return true;
+        if (taken.contains(course)) return false;
+        taken.add(course);
+        for (int prerequisite : pre.get(course)) {
+            if (!dfs(prerequisite, pre, taken)) return false;
+        }
+        taken.remove(course);
+        pre.get(course).clear(); 
+        return true;
+    }
+}
+}
