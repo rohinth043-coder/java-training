17b845054c67118ab2a65823754bdeee79aef902
rohinth043-coder <rohinth043@gmail.com>
2026-07-24 17:17:39 +0530

    rohinth

---
+++ day30/Arrays.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+   Solution() {
+   }
+
+   public int findContentChildren(int[] var1, int[] var2) {
+      Arrays.sort(var1);
+      Arrays.sort(var2);
+      int var3 = 0;
+      byte var4 = 0;
+
+      while(var3 < var1.length && var4 < var2.length) {
+         if (var1[var3] <= var2[var4]) {
+            ++var3;
+         }
+      }
+
+      return var3;
+   }
+}
